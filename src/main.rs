08c8c8ed735b6fb13433f40ff306a8e5c0741d1e
Ferdia McKeogh <ferdia@mckeogh.tech>
2019-07-17 16:24:26 +0200

    enumerated

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use {
     asn1::{
         der::{from_slice, to_vec},
-        types::Enumerable,
+        types::{Enumerable, Enumerated},
     },
     serde::{Deserialize, Serialize},
 };
 
 fn main() {
     env_logger::init();
-    let input = Alpha::A(Bravo::B);
+    let input = Alpha::A(Enumerated::new(Bravo::B));
 
     assert_eq!(input, from_slice(&to_vec(&input).unwrap()).unwrap())
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 enum Alpha {
-    A(Bravo),
-    B(Bravo),
+    A(Enumerated<Bravo>),
+    B(Enumerated<Bravo>),
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -24,3 +24,5 @@ enum Bravo {
     A,
     B,
 }
+
+impl Enumerable for Bravo {}
